d907d83444eeb9c4d1cb60c44c86fda91cdfe7bf
mankamolnar <markmanomolnar@gmail.com>
2017-01-01 16:05:46 +0100

    rotating cube

--- main.py
+++ main.py
@@ -55,6 +55,7 @@ def main():
                 pygame.quit()
                 quit()
 
+        glRotatef(1, 3, 1, 1)
         glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
         Cube()
         pygame.display.flip()
